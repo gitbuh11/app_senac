6a7f0ce5054f18e39df075fcc31e70bcb237a9ee
gitbuh11 <131292446+gitbuh11@users.noreply.github.com>
2023-05-18 16:21:36 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
+/*
 alert("Este aplicativo foi criado por Igor")
+var numero1 = 100;
+var numero2 = 20;
+var resultado = numero1 / numero2;
+alert(resultado)
+
+var curso = {
+    nome : "Programacao",
+    cargaHoraria: 200,
+    alunosPresentes:
+}
+*/
+
+/*var peso = prompt("Qual a sua peso?");
+var altura = prompt("Qual a sua altura?")
+
+var imc = peso / (altura * altura).toFixed(2);
+
+if (imc <= 18.5){
+    alert("MAGREZA")
+}
+else if (imc < 24.9){
+    alert("NORMAL")
+}
+else if (imc < 29.9){
+    alert("SOBREPESO")
+}
+else if (imc < 39.9){
+    alert("OBESIDADE")
+}
+else if (imc >= 39.9){
+    alert("OBEDISADE GRAVE")
+}
+*/
+/*var Lista =["Futebol","Jogos","Aprender","Ler","Viajar"];
+Lista.forEach(hobby => {
+    alert ("Gosto de: " + hobby)
+});
+*/
+
+var curso = {
+    nome: "Programação de Disp. Móveis",
+    conteudo: ["HTML","CSS","JavaScript"],
+    carHoraria: 200
+};
+
+curso.conteudo.forEach(materia=> alert ("No curso de " + curso.nome + "temos aula de:" + materia))
+
+/*
+    conteudo.forEach(c1 => {
+        alert (nome + "temos aula de :"+c1)
+    })
+
+*/
